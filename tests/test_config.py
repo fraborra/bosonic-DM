af0640fd8cc4ed9a6783315f66197b0a7776cd6f
Francesco Borra <borra.francesco@gmail.com>
2026-07-21 07:47:07 -0700

    chore: update configs and tests for new simulation plots

--- tests/test_config.py
+++ tests/test_config.py
@@ -22,7 +22,7 @@ production:
 paths:
   simulation_root: {tmp_path}/simulation
   data_root: {output_root}
-  calibration_dictionaries_root: {calibration_root}
+  inputs_root: {calibration_root}
   plots_root: {tmp_path}/plots
   temporary_root: {tmp_path}/tmp
 analysis:
@@ -30,6 +30,7 @@ analysis:
   fep_window:
     half_width_fwhm: 2.0
   selections: [all]
+  detector_groups: {tmp_path}/groups.yaml
 interactions:
   axio-electric:
     job_template: electron_{{energy}}keV_hpge_bulk
@@ -40,7 +41,6 @@ background:
   comparison_cut_profile: without-bb-like
   energy_ranges_keV: [[20, 300]]
   bin_widths_keV: [5]
-  detector_groups: {tmp_path}/groups.yaml
 output:
   overwrite: false
   save_plots: true
@@ -53,4 +53,4 @@ output:
     config = load_analysis_config(config_path)
 
     assert config.paths.dictionaries_root == output_root / "dictionaries"
-    assert config.paths.calibration_dictionaries_root == calibration_root
+    assert config.paths.inputs_root == calibration_root
